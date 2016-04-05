5cc7afa0bde6f55cd9a1ab7da09bf49c999ac169
didierClin <didier.c62@numericable.fr>
2016-04-05 10:29:35 +0200

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,10 @@ class VisitorsController < ApplicationController
       @owner = Owner.new
       # optionnel car conv over configuration:
       # render 'visitors/new'
+      # Alternative layout appliquer un autre layout
+      # render 'visitors/new', :layout => 'special'
+      flash[:notice] = 'Welcome!'
+      flash.now[:alert] = 'My birthday is soon.'
     end
 
 end
