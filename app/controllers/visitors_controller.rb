5fd502eb2804f86fab620760d2a6ada45363dc33
didierClin <didier.c62@numericable.fr>
2016-04-07 23:00:41 +0200

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,13 +1,31 @@
 class VisitorsController < ApplicationController
 
     def new
-      @owner = Owner.new
+      # @owner = Owner.new
       # optionnel car conv over configuration:
       # render 'visitors/new'
       # Alternative layout appliquer un autre layout
       # render 'visitors/new', :layout => 'special'
       # flash[:notice] = 'Welcome!'
       # flash.now[:alert] = 'My birthday is soon.'
+      @visitor = Visitor.new
     end
+
+    def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:email)
+  end
 
 end
